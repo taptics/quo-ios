f23486a52192d7de16645a40ee1c463836633d22
Ryan Cohen <notryancohen@gmail.com>
2014-10-02 13:51:55 -0400

    Corrected splash screen

--- Quo/LaunchController.m
+++ Quo/LaunchController.m
@@ -6,17 +6,18 @@
 //  Copyright (c) 2014 Taptics. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LaunchController.h"
 
-@interface ViewController ()
+@interface LaunchController ()
 
 @end
 
-@implementation ViewController
+@implementation LaunchController
+
+#pragma mark - View
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
