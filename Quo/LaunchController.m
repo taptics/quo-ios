49eff48c7444e9bd4fbc3245657767dc1489e95b
Ryan Cohen <notryancohen@gmail.com>
2014-10-03 12:23:53 -0400

    Twitter account selection

--- Quo/LaunchController.m
+++ Quo/LaunchController.m
@@ -7,17 +7,71 @@
 //
 
 #import "LaunchController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface LaunchController ()
+
+@property (nonatomic, strong) IBOutlet UIButton *twitterSignUpButton;
+@property (nonatomic, strong) IBOutlet UIButton *signUpLaterButton;
+
+- (IBAction)signUpLater:(id)sender;
+- (IBAction)signUpWithTwitter:(id)sender;
 
 @end
 
 @implementation LaunchController
 
+#pragma mark - Methods
+
+- (IBAction)signUpLater:(id)sender {
+    // Sign up later
+}
+
+- (IBAction)signUpWithTwitter:(id)sender {
+    ACAccountStore *store = [ACAccountStore new];
+    ACAccountType *type = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [store requestAccessToAccountsWithType:type options:nil completion:^(BOOL granted, NSError *error) {
+        if (granted) {
+            NSArray *accounts = [store accountsWithAccountType:type];
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choose Account" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+            UIAlertAction *accountAction = nil;
+            
+            UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+                NSLog(@"Cancelled");
+            }];
+            
+            [alert addAction:cancel];
+            
+            for (ACAccount *account in accounts) {
+                accountAction = [UIAlertAction actionWithTitle:account.username style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                    NSLog(@"Chose: %@", action.title);
+                }];
+                
+                [alert addAction:accountAction];
+            }
+            
+            [self presentViewController:alert animated:YES completion:nil];
+            
+        } else {
+            NSLog(@"Access not granted");
+        }
+    }];
+}
+
 #pragma mark - View
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.twitterSignUpButton.tintColor = [UIColor whiteColor];
+    self.twitterSignUpButton.backgroundColor = [UIColor colorWithRed:255/255.f green:117/255.f blue:80/255.f alpha:1.f];
+    self.twitterSignUpButton.layer.cornerRadius = 4.f;
+    
+    self.signUpLaterButton.tintColor = [UIColor whiteColor];
+    self.signUpLaterButton.backgroundColor = [UIColor colorWithRed:255/255.f green:117/255.f blue:80/255.f alpha:1.f];
+    self.signUpLaterButton.layer.cornerRadius = 4.f;
 }
 
 - (void)didReceiveMemoryWarning {
