3717e309bbd5580dbedba0a9c5c01f859d1c1664
Ryan Cohen <notryancohen@gmail.com>
2014-10-23 15:05:26 -0400

    Home post cell separator now fits all screen sizes

--- Modules/Home Post Cell/HomePostCell.m
+++ Modules/Home Post Cell/HomePostCell.m
@@ -11,15 +11,13 @@
 @implementation HomePostCell
 
 - (void)awakeFromNib {
-    UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(0, self.contentView.frame.size.height + 90, self.contentView.frame.size.width, 1)];
+    UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(0, self.contentView.frame.size.height + 90, CGRectGetWidth([UIScreen mainScreen].bounds), 1)];
     separator.backgroundColor = [UIColor colorWithRed:225/255.f green:225/255.f blue:225/255.f alpha:1.f];
     [self.contentView addSubview:separator];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
